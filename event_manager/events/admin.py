5e3f06054ae2a999da618ba88fb504540c33851e
Dillon Harville <dharville1257@gmail.com>
2024-11-27 14:39:37 -0500

    refactoring

--- event_manager/events/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Event
-
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date', 'max_slots', 'slots_filled', 'slots_available', 'is_full')
-    list_filter = ('date',)
-    search_fields = ('title', 'description')
--- event_manager/mailing_list/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('subscribe/', views.subscribe, name='subscribe'),  # Form to subscribe to the mailing list
-    path('unsubscribe/', views.unsubscribe, name='unsubscribe'),  # Form to unsubscribe from the mailing list
-]
